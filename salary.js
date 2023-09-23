dcc89d1ce9cd10d754413ac042ac98f0ad11befc
Maryannewambui <trimbleleewambui@gmail.com>
2023-09-23 10:00:31 +0300

    challenge 3 complete

---
+++ salary.js
@@ -0,0 +1,32 @@
+//program that calculates the net salary
+
+function calculatesNetSalary(basicSalary, benefits) {
+     // Prompt the user to input the basic salary and benefits
+     const basicSalary = parseFloat(prompt("Enter Basic Salary:"));
+     const benefits = parseFloat(prompt("Enter Benefits:"));
+
+     //Gross salary
+     const grossSalary = basicSalary + benefits;
+     console.log("Gross Salary: " + salary.grossSalary);
+
+     //tax rate
+     const kraTaxRate = 0.3; // 30% rate
+     const nhifRate = 0.02; // 2% rate
+     const nssfRate = 0.06; // 6%  rate
+
+       // Calculate deductions
+       const kraDeduction = grossSalary * kraTaxRate;
+       console.log("KRA Deduction: " + salary.kraDeduction);
+
+       const nhifDeduction = grossSalary * nhifRate;
+       console.log("NHIF Deduction: " + salary.nhifDeduction);
+
+       const nssfDeduction = grossSalary * nssfRate;
+       console.log ("NHIF Deduction: " + salary.nhifDeduction);
+
+       //net salary
+       const netSalary = grossSalary - kraDeduction - nhifDeduction - nssfDeduction;
+       return`Net salary is: ${netSalary}`;
+
+}
+const salary = calculateSNetSalary(basicSalary, benefits);
